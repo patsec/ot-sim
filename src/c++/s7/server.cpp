d3dbbd850d06ca3490f4e43c65a5fc379bd49004
Hunter Toohey <htoohey@nrel.gov>
2024-07-05 14:39:34 -0700

    remove dnp3 functions from s7::server.hpp, start implementing s7::server.cpp

--- src/c++/s7/server.cpp
+++ src/c++/s7/server.cpp
@@ -5,5 +5,24 @@
 namespace otsim {
 namespace s7 {
 
+void Outstation::HandleMsgBusStatus(const otsim::msgbus::Envelope<otsim::msgbus::Status>& env) {
+  auto sender = otsim::msgbus::GetEnvelopeSender(env);
+
+  if (sender == config.id) {
+    return;
+  }
+
+  metrics->IncrMetric("status_count");
+
+  for (auto &p : env.contents.measurements) {
+    if (points.count(p.tag)) {
+      std::cout << fmt::format("[{}] status received for tag {}", config.id, p.tag) << std::endl;
+
+      auto lock = std::unique_lock<std::mutex>(pointsMu);
+      points[p.tag] = p;
+    }
+  }
+}
+
 } // namespace s7
 } // namespace otsim
