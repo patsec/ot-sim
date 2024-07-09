ee3653799621861df73842a8e0dd9a8944a9bd09
Hunter Toohey <htoohey@nrel.gov>
2024-07-09 14:21:50 -0700

    finish HandleMsgBusUpdate function

--- src/c++/s7/client.cpp
+++ src/c++/s7/client.cpp
@@ -15,7 +15,8 @@ void Client::HandleMsgBusUpdate(const otsim::msgbus::Envelope<otsim::msgbus::Upd
     if (sender == id) {
         return;
     }
-
+    
+    //sort through each point in the envelope and handle them with appropriate functions for binary and analog
     for (auto &p : env.contents.updates) {
         if (WriteBinary(p.tag,p.value)) {
             continue;
